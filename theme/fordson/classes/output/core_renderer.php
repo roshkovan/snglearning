eb4af92c3c66adc5ea6d0ecad840ebc5569b0cc4
Roshkovan Kirill <Roshkovan_KS@surgutneftegas.ru>
2018-10-03 23:51:01 +0500

    mod theme_fordson

--- theme/fordson/classes/output/core_renderer.php
+++ theme/fordson/classes/output/core_renderer.php
@@ -1044,7 +1044,7 @@ class core_renderer extends \theme_boost\output\core_renderer {
         $editcog = html_writer::div($this->context_header_settings_menu() , 'pull-xs-right context-header-settings-menu');
         $siteadmintitle = get_string('siteadminquicklink', 'theme_fordson');
         $siteadminurl = new moodle_url('/admin/search.php');
-        $hasadminlink = is_siteadmin();
+        $hasadminlink = has_capability('moodle/user:create', $context);
         $course = $this->page->course;
         // Send to template.
         $dashmenu = ['showincourseonly' => $showincourseonly, 'togglebutton' => $togglebutton, 'togglebuttonstudent' => $togglebuttonstudent, 'hasteacherdash' => $hasteacherdash, 'hasstudentdash' => $hasstudentdash, 'haspermission' => $haspermission, 'hasadminlink' => $hasadminlink, 'siteadmintitle' => $siteadmintitle, 'siteadminurl' => $siteadminurl, 'haseditcog' => $haseditcog, 'editcog' => $editcog, ];
@@ -1119,8 +1119,8 @@ class core_renderer extends \theme_boost\output\core_renderer {
                 'type' => 'easy'
             ));
         }
-        $gradestitle = get_string('gradebooksetup', 'grades');
-        $gradeslink = new moodle_url('/grade/edit/tree/index.php', array(
+        $gradestitle = get_string('gradebook', 'grades');
+        $gradeslink = new moodle_url('/grade/report/grader/index.php', array(
             'id' => $PAGE->course->id
         ));
         $participantstitle = ($PAGE->theme->settings->studentdashboardtextbox == 1) ? false : get_string('participants', 'moodle');
